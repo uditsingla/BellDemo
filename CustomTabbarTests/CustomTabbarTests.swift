7dcc5da671eec967b6afe3ed5972d7fdb4fb4dbc
Abhishek Singla <abhisheksingla@Abhisheks-MacBook-Pro-2.local>
2018-06-14 16:36:25 +0530

    Test

--- CustomTabbarTests/CustomTabbarTests.swift
+++ CustomTabbarTests/CustomTabbarTests.swift
@@ -20,7 +20,7 @@ class CustomTabbarTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    /*
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -37,7 +37,7 @@ class CustomTabbarTests: XCTestCase {
     func testLoginSuccess(){
         //XCTAssertEqual(1, 1)
     }
- */
+ 
     
     
     
